6a6ee28e264799dbaff6c0b22de64f2ea09a8774
Flavio Auciello <flavioauciello@nebulab.it>
2018-07-19 22:28:25 +0200

    v0.0.1
    + implement little testing for retrieve a page (both HTML/XML) and
      parse it.

--- lib/elinews.ex
+++ lib/elinews.ex
@@ -1,18 +1,49 @@
+require IEx
 defmodule Elinews do
   @moduledoc """
   Documentation for Elinews.
   """
+  @news_feed "http://www.tgcom24.mediaset.it/rss/ultimissime.xml"
 
   @doc """
-  Hello world.
+  Retrieve news feed XML file and extract news informations.
 
   ## Examples
 
-      iex> Elinews.hello
-      :world
+  iex> Elinews.news_feed
+  "http://www.tgcom24.mediaset.it/rss/ultimissime.xml"
 
   """
-  def hello do
-    :world
+
+  def news_feed do
+    @news_feed
+  end
+
+  def retrieve_news(criteria) do
+    {field, keyword} = hd criteria
+    retrieve_news()
+    |> filter(field, keyword)
+  end
+
+  def retrieve_news do
+    HTTPotion.get(@news_feed, follow_redirects: true).body
+    |> Floki.find("item")
+    |> Enum.map(&item_map(&1))
+  end
+
+  defp item_map(item) do
+    {"item", [], [
+        {"title", [], title},
+        {"description", [], [description]},
+        {"link", [], [link]}
+        | _
+      ]
+    } = item
+    %{title: hd(title), description: description, link: link}
+  end
+
+  defp filter(items, field, value) do
+    items
+    |> Enum.filter(fn(i) -> Regex.run(~r/#{value}/i, i[field]) end)
   end
 end
--- test/elinews_test.exs
+++ test/elinews_test.exs
@@ -2,7 +2,13 @@ defmodule ElinewsTest do
   use ExUnit.Case
   doctest Elinews
 
-  test "greets the world" do
-    assert Elinews.hello() == :world
+  @tag timeout: 120_000 # Override to infinity with `mix test --trace`
+
+  test "Retrieve news with no arguments" do
+    assert length(Elinews.retrieve_news) > 0
+  end
+
+  test "Retrive news with a filter" do
+    assert length(Elinews.retrieve_news(title: 'trump')) > 0
   end
 end
